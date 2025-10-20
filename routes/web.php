bb46b481a8ee76dbf9ec1e165c79e513e2780f2d
Tolu <tlawal86@hotmail.co.uk>
2025-10-20 13:51:40 +0100

    can create and delete papers

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,21 @@ Route::get('/', [WorkspaceController::class, 'home'])->name('home');
 //show a singular workspace and its papers
 Route::get('/workspace/{id}', [WorkspaceController::class, 'workspace'])->name('workspace.show');
 
-//show all notes belonging to a paper
-Route::get('/workspace/{workspace}/paper/{paper}', [PaperController::class, 'show'])->name('paper.show')->scopeBindings();
+//Create a workspace
+Route::post('/workspace', [WorkspaceController::class, 'store'])->name(('workspace.store'));
 
 //Delete a workspace
 Route::delete('/{workspace}', [WorkspaceController::class, 'destroy'])->name('workspace.destroy');
 
-//Create a workspace
-Route::post('/workspace', [WorkspaceController::class, 'store'])->name(('workspace.store'));
-
-//Delete a paper
-Route::delete('/workspaces/{workspace}/papers/{paper}', [PaperController::class, 'destroy'])->name('paper.destroy')->scopeBindings();
 
 //create a paper
 Route::post('/{workspace}', [PaperController::class, 'store'])->name('paper.store')->scopeBindings();
 
+//Delete a paper
+Route::delete('/workspaces/{workspace}/papers/{paper}', [PaperController::class, 'destroy'])->name('paper.destroy')->scopeBindings();
+
+//show all notes belonging to a paper
+Route::get('/workspace/{workspace}/paper/{paper}', [PaperController::class, 'show'])->name('paper.show')->scopeBindings();
 
 
 Route::get('/wys', function () {
